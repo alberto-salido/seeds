3197505b0a7db0b174eda06e91c0d15567578f7b
Alberto Salido López <alberto.026@gmail.com>
2021-07-13 23:28:26 +0200

    Create seeds.py
    Initial commit.

---
+++ seeds/seeds.py
@@ -0,0 +1,16 @@
+from linkedin_api import Linkedin
+
+import json
+
+SEARCH_USER_MSG = "Nombre de la persona a buscar en LinkedIn:\n"
+USER_PROFILE_MSG = "Nombre el identificador de LinkedIn:\n"
+SELECT_OPTION_MSG = "Selecciona una opción:\n 1) Buscar usuarios en Linkedin.\n 2) Obtener información de un usuario.\n 0) Salir.\n"
+
+username = ""
+password = ""
+
+while True:
+    print("hey")
+    break
+
+
